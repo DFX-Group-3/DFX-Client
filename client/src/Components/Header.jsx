40af20beb4580f37259897f437be5c53dc04b3a2
OneOverCosine <TheCandyDragon@gmail.com>
2023-03-16 14:24:57 +0000

    added Header component

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+    return (
+        <>
+            <div className="links">
+                <a href="#">Github</a>
+                <a href="#">LinkedIn</a>
+                <a href="#">Youtube</a>
+            </div>
+
+            <div>
+                <h2>Name Surname</h2>
+                <p>pronouns</p>
+                <p>tagline</p>
+                <i class="fas fa-edit"></i>
+            </div>
+
+            <div>
+                <h3>Overview</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iusto voluptatum temporibus odit cupiditate quos necessitatibus beatae quibusdam vel debitis soluta dolor delectus culpa, aut placeat repellat harum illo eaque adipisci ducimus maiores veritatis? Facilis, voluptatem et dicta, consequuntur quibusdam recusandae quod reiciendis nesciunt nam iure magnam maiores beatae cupiditate!</p>
+            </div>
+        </>
+    );
+}
+
+export default Header
