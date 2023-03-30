cc56978a9100f5f475d7acd414ad738d4b570391
Krisztian <krisztian.valyi1@gmail.com>
2023-03-30 13:38:13 +0100

    Added expand only specific experience item

--- client/src/Components/Profile/Experience/ExperienceItem/ExperienceItem.jsx
+++ client/src/Components/Profile/Experience/ExperienceItem/ExperienceItem.jsx
@@ -1,81 +1,81 @@
-import React, { useState, useEffect } from 'react'
-import './ExperienceItem.css'
-import axios from 'axios'
-import { useUserContext } from '../../../../hooks/UseUserContext'
-
+import React, { useState, useEffect } from 'react';
+import './ExperienceItem.css';
+import axios from 'axios';
+import { useUserContext } from '../../../../hooks/UseUserContext';
 
 export default function ExperienceItem(props) {
-  const [showExpand, setShowExpand] = useState(false)
-  // const { imageURL, organization, priority, position, description, startDate, endDate } = props.experience.experience
-
-  const [experience, setExperience] = useState([])
-
-  const { user } = useUserContext()
+  const { user } = useUserContext();
+  const [experience, setExperience] = useState([]);
 
   useEffect(() => {
     const getExperience = async () => {
-      const uri = `http://localhost:9000/experience`
+      const uri = `http://localhost:9000/experience`;
 
       const response = await axios.get(uri, {
         headers: {
-          'Authorization': `Bearer ${user.token}`
-        }
-      })
-      setExperience(response.data)
-    }
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      setExperience(
+        response.data.map((exp) => ({
+          ...exp,
+          showExpand: false,
+        }))
+      );
+    };
     getExperience();
-  }, [])
-  const {
-    imageURL,
-    organization,
-    priority,
-    position,
-    description,
-    startDate,
-    endDate
-  } = experience;
-console.log(experience)
+  }, []);
+
+  const toggleExpand = (id) => {
+    setExperience((prevExperience) =>
+      prevExperience.map((exp) =>
+        exp._id === id ? { ...exp, showExpand: !exp.showExpand } : exp
+      )
+    );
+  };
+
   const experiences = experience.map((exp) => {
-    const { organization, position, startDate, endDate, description, imageURL } = exp;
+
+    const { _id, organization, position, startDate, endDate, description, imageURL, showExpand } = exp;
+
+    let formatStartDate = new Date(startDate);
+    let formatEndDate = new Date(endDate);
+
     return (
-    <div key={exp._id}>
-      <div  className="row exp-item-row">
-        <div className="col-4 company-col">
-          <div className='col-6'><img alt="lorem picsum" src={imageURL} /></div>
-          <div className='col-5'>{organization}</div>
-
-        </div>
-
-        <div className="col-4 pos-name-col">{position}</div>
-        <div className='col-2'>{startDate} - {endDate}</div>
-        <div className='col-1 expand'><button onClick={() => setShowExpand(!showExpand)}>{!showExpand ? "Expand" : "Collapse"}</button></div>
-      </div>
-      {showExpand &&
-        <div className='expand-row'>
-          <div className='title-bts'>
-            <h6 className='title'>{position}</h6>
-            <div className='buttons'>
-              <button>
-                <img src='https://cdn-icons-png.flaticon.com/512/1159/1159633.png' />
-              </button>
-
+      <div key={_id}>
+        <div className="row exp-item-row">
+          <div className="col-4 company-col">
+            <div className="col-6">
+              <img alt="lorem picsum" src={imageURL} />
             </div>
+            <div className="col-5">{organization}</div>
           </div>
+
+          <div className="col-4 pos-name-col">{position}</div>
+          <div className="col-2">
+            {formatStartDate.toLocaleDateString()} - {formatEndDate.toLocaleDateString()}
+          </div>
+          <div className="col-1 expand">
+            <button onClick={() => toggleExpand(_id)}>{!showExpand ? "Expand" : "Collapse"}</button>
+          </div>
+        </div>
+        {showExpand && (
+          <div className="expand-row">
+            <div className="title-bts">
+              <h6 className="title">{position}</h6>
+              <div className="buttons">
+                <button>
+                  <img src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png" alt='update button'/>
+                </button>
+              </div>
+            </div>
             <p>{description}</p>
             {/* needs fix in database, to have skills as well */}
-          <p>Skills: </p>
+          </div>
+        )}
+      </div>
+    );
+  });
 
-
-        </div>}
-    </div>
-
-  )
-  })
-
-  return (
-    <>
-      {experiences}
-    </>
-
-  )
+  return <>{experiences}</>;
 }
